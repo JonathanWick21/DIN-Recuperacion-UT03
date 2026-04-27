68d0f1abdab0618b2f28d946cb4ca0e612d5ad53
JonathanWick21 <jonathanwick2110@gmail.com>
2026-04-27 19:19:01 +0200

    feat: Pagina Principal, Catalogo y Detalle

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Catalogue from './pages/Catalogue';
+import Detail from './pages/Detail';
+import AdminPage from './pages/AdminPage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/catalogo' element={<Catalogue />} />
+        <Route path='detalle/:id' element={<Detail />} />
+        <Route path='admin' element={<AdminPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
